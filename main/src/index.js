517e25a0638efb648a315fdeda6f3ac289e95ebf
Yago Macinelli <yagom.ym@gmail.com>
2023-03-06 15:44:21 -0300

    Do: => Fragmentation functions darkMode and whiteMode.

--- main/src/index.js
+++ main/src/index.js
@@ -2,16 +2,34 @@
 function changeTheme() {
     let theme = document.body.classList[0];
     if (theme === "white-theme") {
-        document.getElementById("img-contrast").src = "/main/assets/icon/sunny-outline.svg";
-        document.getElementById("img-contrast").classList.add("invert-1");
-        document.body.classList = "dark-theme";
+        darkMode();
     } else {
-        document.getElementById("img-contrast").src = "/main/assets/icon/moon-outline.svg";
-        document.getElementById("img-contrast").classList.remove("invert-1");
-        document.body.classList = "white-theme";
+        whiteMode();
     }
 }
 
+function whiteMode() {
+    document.body.classList = "white-theme";
+
+    document.getElementById("img-contrast").src = "/main/assets/icon/moon-outline.svg"; //icon-mode
+    document.getElementById("img-contrast").classList.remove("invert-1"); //icon-mode
+
+    document.styleSheets.item(0).cssRules.item(11).style.backgroundColor = "#eeeeee"; //input[type=text]
+    document.styleSheets.item(0).cssRules.item(11).style.color = "#000000"; //input[type=text]
+    document.styleSheets.item(0).cssRules.item(14).style.color = "#000000"; //label-radio
+}
+
+function darkMode() {
+    document.body.classList = "dark-theme";
+
+    document.getElementById("img-contrast").src = "/main/assets/icon/sunny-outline.svg"; //icon-mode
+    document.getElementById("img-contrast").classList.add("invert-1"); //icon-mode
+
+    document.styleSheets.item(0).cssRules.item(11).style.backgroundColor = "#222222"; //input[type=text]
+    document.styleSheets.item(0).cssRules.item(11).style.color = "#ffffff"; //input[type=text]
+    document.styleSheets.item(0).cssRules.item(14).style.color = "#ffffff"; //label-radio
+}
+
 function onlyNumbers(inputId) {
-    document.getElementById(inputId).value = document.getElementById(inputId).value.replace(/\D,/g, "");
+    document.getElementById(inputId).value = document.getElementById(inputId).value.replace(/\D/g, "");
 }
