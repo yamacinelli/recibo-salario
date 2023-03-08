2d293f22dfadb2b3538fbf08f2bcc92a84d2dd0a
Yago Macinelli <yagom.ym@gmail.com>
2023-03-08 11:08:15 -0300

    Do: => Added function to release field Chave PIX.

--- main/src/page/index/index.js
+++ main/src/page/index/index.js
@@ -33,7 +33,8 @@ function darkMode() {
 }
 
 function onlyNumbers(inputId) {
-    document.getElementById(inputId).value = document.getElementById(inputId).value.replace(/^[\D,]/g, "");
+    document.getElementById(inputId).value = document.getElementById(inputId).value.replace(/[^\,0-9]*$/, "");
+    if (inputId === "cpf_cnpj") cpfCnpjFormat()
 }
 
 function currencyFormat() {
@@ -43,6 +44,19 @@ function currencyFormat() {
         maximumFractionDigits: 2,
         useGrouping: true,
     });
-    const value = document.getElementById("valor").value.replace(',', '.');
+    const value = document.getElementById("valor").value.replace(',', '.').replace(/^[\D\.]+/g, '');
     document.getElementById("valor").value = formatter.format(value);
+}
+
+function cpfCnpjFormat() {
+    const value = document.getElementById("cpf_cnpj").value.replace(/\D/g, "");
+    value.length > 11
+        ? document.getElementById("cpf_cnpj").value = value.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5")
+        : document.getElementById("cpf_cnpj").value = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+}
+
+function radioSelected(radio) {
+        radio.value === "PIX" && radio.checked ?
+            document.styleSheets.item(0).cssRules.item(27).style.display = "block" :
+            document.styleSheets.item(0).cssRules.item(27).style.display = "none"; //row-chave
 }
